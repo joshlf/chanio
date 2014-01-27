1c7878ce566397a8d2a05b6b868a1f5958dbac31
Joshua Liebow-Feeser <hello@joshlf.com>
2014-01-27 03:10:21 -0500

    Added documentation.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 chanio
 ======
 
-Conversions between channels and io types
+The chanio package allows for using the io.Reader, io.Writer, and io.Closer interfaces to interact with byte channels.
+
+See the [documentation](http://godoc.org/github.com/joshlf13/chanio).
--- chanio.go
+++ chanio.go
@@ -2,14 +2,28 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// The chanio package allows for using the io.Reader,
+// io.Writer, and io.Closer interfaces to interact with
+// byte channels.
 package chanio
 
 import (
 	"io"
 )
 
+// Reader implements the io.Reader interface.
 type Reader <-chan byte
 
+// Read reads bytes from r into p, blocking until at least
+// 1 byte is available. After that, it reads as many bytes
+// into p as it can without blocking, or until p is full.
+//
+// If r is closed, Read will return io.EOF. If there are any
+// bytes available to be read from r, it will return those
+// bytes and a nil error, and return io.EOF on the subsequent
+// call.
+//
+// If len(p) == 0, Read returns 0 and a nil error immediately.
 func (r Reader) Read(p []byte) (int, error) {
 	if len(p) < 1 {
 		return 0, nil
@@ -35,8 +49,12 @@ func (r Reader) Read(p []byte) (int, error) {
 	return n, nil
 }
 
+// WriteCloser implements the io.WriteCloser interface.
 type WriteCloser chan<- byte
 
+// Write writes bytes from p into w, blocking until all
+// bytes have been written. Write will never return a
+// non-nil error.
 func (w WriteCloser) Write(p []byte) (int, error) {
 	if len(p) < 1 {
 		return 0, nil
@@ -47,21 +65,37 @@ func (w WriteCloser) Write(p []byte) (int, error) {
 	return len(p), nil
 }
 
+// Close closes w. It will never return a non-nil error.
 func (w WriteCloser) Close() error {
 	close(w)
 	return nil
 }
 
+// ReadWriteCloser implements the io.ReadWriteCloser interface.
 type ReadWriteCloser chan byte
 
+// Read reads bytes from r into p, blocking until at least
+// 1 byte is available. After that, it reads as many bytes
+// into p as it can without blocking, or until p is full.
+//
+// If r is closed, Read will return io.EOF. If there are any
+// bytes available to be read from r, it will return those
+// bytes and a nil error, and return io.EOF on the subsequent
+// call.
+//
+// If len(p) == 0, Read returns 0 and a nil error immediately.
 func (r ReadWriteCloser) Read(p []byte) (int, error) {
 	return Reader(chan byte(r)).Read(p)
 }
 
+// Write writes bytes from p into w, blocking until all
+// bytes have been written. Write will never return a
+// non-nil error.
 func (r ReadWriteCloser) Write(p []byte) (int, error) {
 	return WriteCloser(chan byte(r)).Write(p)
 }
 
+// Close closes w. It will never return a non-nil error.
 func (r ReadWriteCloser) Close() error {
 	return WriteCloser(chan byte(r)).Close()
 }
